eb1c15fbd207e49d36b00ae8acb7fd4ff4492de2
quepas <kiepas.patryk@gmail.com>
2020-02-13 16:14:06 +0100

    Using default output file, if only needed.

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,29 @@
 
 int main(int argc, char **argv) {
     std::ifstream trace_file;
+    std::string pep_file_path, csv_file_path = "./output.csv";
     if (argc > 1) {
-        std::string trace_file_path = argv[1];
-        std::cout << "Input trace file=" << trace_file_path << std::endl;
-        if (!std::filesystem::exists(trace_file_path)) {
-            std::cerr << "Trace file doesn't exist" << std::endl;
+        pep_file_path = argv[1];
+        if (!std::filesystem::exists(pep_file_path)) {
+            std::cerr << "PEP file doesn't exist: " << pep_file_path << std::endl;
             return EXIT_FAILURE;
         }
-        trace_file.open(trace_file_path);
+        trace_file.open(pep_file_path);
         if (!trace_file.is_open()) {
-            std::cerr << "Couldn't open file" << std::endl;
+            std::cerr << "Couldn't open PEP file: " << pep_file_path << std::endl;
             return EXIT_FAILURE;
         }
+    } else {
+        std::cerr << "Missing path to the PEP file." << std::endl;
+        return EXIT_FAILURE;
     }
+    if (argc > 2) {
+        csv_file_path = argv[2];
+    } else {
+        std::cout << "Using default CSV output path: ./output.csv" << std::endl;
+    }
+
+    std::cout << "pep2csv: " << pep_file_path << " --> " << csv_file_path << std::endl;
 
     // Parse the input trace
     antlr4::ANTLRInputStream input(trace_file);
@@ -34,7 +44,7 @@ int main(int argc, char **argv) {
     antlr4::tree::ParseTree *tree = parser.pep();
 
     antlr4::tree::IterativeParseTreeWalker tree_walker;
-    PEPTracesListener pep_listener;
+    PEPTracesListener pep_listener(csv_file_path);
     tree_walker.walk(&pep_listener, tree);
 
     return EXIT_SUCCESS;
